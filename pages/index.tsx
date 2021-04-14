47a1901ed4572388d915f9e827145195330a2f75
Matheus Bittencourt <matheus@ae.studio>
2021-04-13 22:04:08 -0300

    add sample styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+import {
+  Box,
+  Typography,
+  makeStyles,
+} from '@material-ui/core'
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: 100,
+  },
+})
 
 const Home: React.FunctionComponent = () => {
-  return <Typography>Hello World!</Typography>
+  const styles = useStyles()
+
+  return (
+    <Box className={styles.container}>
+      <Typography variant='h2'>Hello World!</Typography>
+    </Box>
+  )
 }
 
 export default Home
